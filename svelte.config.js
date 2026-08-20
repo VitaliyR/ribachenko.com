93efe13f1cc82df9ad3d26376c60f67221c807f6
Vitalii Rybachenko <vit@ribachenko.com>
2026-08-20 14:14:56 -0400

    Redirects

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-import nodeAdapter from '@sveltejs/adapter-node';
-import netlifyAdapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
@@ -9,10 +8,6 @@ import images from './src/build/images.mjs';
 
 const filePath = dirname(fileURLToPath(import.meta.url));
 const stylePath = `${filePath}/src/styles`;
-
-const adapter = process.env.NETLIFY
-  ? netlifyAdapter()
-  : nodeAdapter({ precompress: true });
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -35,7 +30,7 @@ const config = {
   ],
 
   kit: {
-    adapter
+    adapter: adapter()
   }
 };
 
